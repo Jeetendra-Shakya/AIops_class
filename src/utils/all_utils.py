ee08c9a6c8dd14738c4836890daf9063f096b44d
jitendra <jitendra_shakya@hotmail.com>
2021-10-07 15:08:18 +0100

    Stage 02 added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -13,3 +13,8 @@ def create_directory(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path,exist_ok=True)
         print(f"directory created at {dir_path}")
+
+
+def save_local_df(data, data_path, index_status=False):
+    data.to_csv (data_path, index= index_status)
+    print(f"Data is save at {data_path}")
